bd6db926fe1423884517a5ed59feca1b5ee55231
Radosław Kowalski <ladekesl@gmail.com>
2023-07-04 21:35:05 +0200

    Refactor AdRecord constructor for property assignment

--- records/ad.record.ts
+++ records/ad.record.ts
@@ -6,7 +6,7 @@ import {FieldPacket} from "mysql2";
 type AdRecordResults = [AdEntity[], FieldPacket[]];
 
 
-export class AdRecord implements AdEntity{
+export class AdRecord implements AdEntity {
     id: string;
     name: string;
     description: string;
@@ -16,37 +16,36 @@ export class AdRecord implements AdEntity{
     lon: number;
 
     constructor(obj: NewAdEntity) {
-        if(!obj.name || obj.name.length > 100) {
+        const {name, description, id, url, lat, lon, price} = obj;
+
+        if (!obj.name || obj.name.length > 100) {
             throw new ValidationError('Nazwa ogłoszenia nie może byc pusta, ani przekraczać 100 znaków.');
         }
-        if(obj.description.length > 1000) {
+        if (obj.description.length > 1000) {
             throw new ValidationError('Treść ogłoszenia nie może przekraczać 1000 znaków.');
         }
-        if(obj.price < 0 || obj.price > 999999) {
+        if (obj.price < 0 || obj.price > 999999) {
             throw new ValidationError('Cena nie może być mniejsza niż 0 lub większa niz 99999.');
         }
-        if(!obj.url || obj.url.length > 100) {
+        if (!obj.url || obj.url.length > 100) {
             throw new ValidationError('Link ogłoszenia nie może być pusty, ani przekraczać 100 znaków.');
         }
-        if( typeof obj.lat !== "number" || typeof obj.lon !== "number") {
+        if (typeof obj.lat !== "number" || typeof obj.lon !== "number") {
             throw new ValidationError('Nie można zlokalizować ogłoszenia.');
         }
 
-        this.name = obj.name;
-        this.description = obj.description;
-        this.id = obj.id;
-        this.url = obj.url;
-        this.lat = obj.lat;
-        this.lon = obj.lon;
-        this.price = obj.price;
-
-
-
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.url = url;
+        this.lat = lat;
+        this.lon = lon;
+        this.price = price;
 
     }
 
     static async getOne(id: string): Promise<AdRecord | null> {
-        const[results] = await pool.execute("SELECT * FROM `ads` WHERE id = :id", {
+        const [results] = await pool.execute("SELECT * FROM `ads` WHERE id = :id", {
             id,
         }) as AdRecordResults;
         return results.length === 0 ? null : new AdRecord(results[0]);
