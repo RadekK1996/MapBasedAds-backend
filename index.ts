147554c19b5ae5f3801e5f184883c4dc31eb52fe
Radosław Kowalski <ladekesl@gmail.com>
2023-07-19 20:09:57 +0200

    Install necessary packages and create ormconfig.json

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json, Router} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import 'reflect-metadata';
 import {handleError} from "./utils/errors";
 import rateLimit from "express-rate-limit";
 import {adRouter} from "./routers/ad.router";
